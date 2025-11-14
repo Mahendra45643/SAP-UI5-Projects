7fbf46b1ecea207206dc494bd302365f5672dec1
Mahendra Pandrameeesu <pandrameesumahendra@gmail.com>
2025-11-14 12:24:08 +0000

    model and i18n properites are added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,44 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],(Controller) =>{
-        "use strict"
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/model/resource/ResourceModel"
+    ],
+    (Controller, JSONModel, ResourceModel) => {
+        "use strict";
+        return Controller.extend("ui5.walkthrough.controller.App", {
 
-        return Controller.extend("ui5.walkthrough.controller.App",{
-           onPress: () =>{
-            console.log("ButtonPressed");
-            alert("Say Hi") 
+            // JSONModel set handler
+            onInit() {
+                const oModel = new JSONModel({
+                    recipent: {
+                        name: "Mahendra Pandrameesu"
+                    }
+                });
 
-            setTimeout(async()=>{
-             console.log("onPress async call function excuted after 3 secs");
-            },3000)
+                this.getView().setModel(oModel);
 
-           }
-        })
-    
-})
+                // i18n properties ResourceModel
+                const oResourceModel = new ResourceModel({
+                    bundleName: "ui5.walkthrough.i18n.i18n"
+                });
+
+                this.getView().setModel(oResourceModel, "translableTexts");
+            },
+
+            onAfterRendering() {
+            },
+
+            // press event handler
+            onPress() {
+                console.log("ButtonPressed");
+                alert("Say Hi");
+
+                setTimeout(async () => {
+                    console.log("onPress async call function excuted after 3 secs");
+                }, 3000);
+            }
+
+        });
+    }
+);
